ba2b24a945b6cf3e37211533aed7141c1b35ea7e
Fille Åström <fille@imgnry.com>
2014-05-30 11:30:12 +0200

    Initial Commit

---
+++ NetworkLab/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NetworkLab
+//
+//  Created by Fille Åström on 30/05/14.
+//  Copyright (c) 2014 IMGNRY International AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NetworkLab/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NetworkLab
+//
+//  Created by Fille Åström on 30/05/14.
+//  Copyright (c) 2014 IMGNRY International AB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
